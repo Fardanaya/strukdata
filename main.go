fe976dfa4037cb75f355696930bb356edf7a5e8c
Fardanaya <frankyardanaya@gmail.com>
2023-04-06 19:53:10 +0700

    sudah masokk tapi bug e podo

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strings"
 )
 
 type Dosen struct {
@@ -80,11 +81,11 @@ func ReadAll(listDosen *structDosen, listMahasiswa *structMahasiswa, selector in
 		}
 	} else {
 		for tempMahasiswa != nil {
-			fmt.Println("-Data Dosen-")
+			fmt.Println("-Data Mahasiswa-")
 			fmt.Println("-ID\t\t : ", tempMahasiswa.data.id)
 			fmt.Println("-Nama\t : ", tempMahasiswa.data.nama)
 			fmt.Println("-Alamat\t : ", tempMahasiswa.data.alamat)
-			fmt.Println("-NIP\t : ", tempMahasiswa.data.nim)
+			fmt.Println("-NIM\t : ", tempMahasiswa.data.nim)
 			fmt.Println("-No. HP\t : ", tempMahasiswa.data.hp)
 			fmt.Println("-TTL\t : ", tempMahasiswa.data.ttl)
 			tempMahasiswa = tempMahasiswa.next
@@ -136,11 +137,14 @@ func DeleteData(listDosen *structDosen, listMahasiswa *structMahasiswa, selector
 
 func inputUser(selector int) {
 	//var nama, alamat, ttl, hp, nip, nim string
-	var nim string
+	var x string
 	scanner := bufio.NewScanner(os.Stdin)
-
-	if selector == 1 {
-		fmt.Println("Masukkan data dosen")
+	for x != "t" {
+		if selector == 1 {
+			fmt.Println("Masukkan data dosen")
+		} else {
+			fmt.Println("Masukkan data mahasiswa")
+		}
 		fmt.Print("Nama\t: ")
 		scanner.Scan()
 		nama := scanner.Text()
@@ -155,32 +159,95 @@ func inputUser(selector int) {
 		hp := scanner.Text()
 		fmt.Print("NIP\t: ")
 		scanner.Scan()
-		nip := scanner.Text()
+		ni := scanner.Text()
+		dataDosen := Dosen{id, nama, alamat, ttl, hp, ni}
+		dataMahasiswa := Mahasiswa{id, nama, alamat, ttl, hp, ni}
+		InsertData(&dosen, &mahasiswa, dataDosen, dataMahasiswa, selector)
 
-		dataDosen := Dosen{id, nama, alamat, ttl, hp, nip}
-		dataMahasiswa := Mahasiswa{id, nama, alamat, ttl, hp, nim}
-		InsertData(&dosen, nil, dataDosen, dataMahasiswa, 1)
+		fmt.Println("TAMBAH DATA ?(y/t)")
+		scanner.Scan()
+		x = strings.TrimSpace(scanner.Text())
+		// if x == "t" {
+		// 	break
+		// }
+
+	}
+
+}
+
+func utama() {
+	var in int
+	for in != 3 {
+		fmt.Println("PENGISIAN DATA")
+		fmt.Println("1. DOSEN")
+		fmt.Println("2. MAHASISWA")
+		fmt.Println("3. EXIT")
+		fmt.Scan(&in)
+		if in != 3 {
+			menu(in)
+		}
+	}
+}
+
+func menu(x int) {
+	var in int
+	for in != 6 {
+		switch x {
+		case 1:
+			fmt.Println("PENGISIAN DATA DOSEN")
+		case 2:
+			fmt.Println("PENGISIAN DATA MAHASISWA")
+		}
+		fmt.Println("1. Tambah data")
+		fmt.Println("2. Hapus data")
+		fmt.Println("3. Update data")
+		fmt.Println("4. Lihat semua data")
+		fmt.Println("5. Lihat dari ID")
+		fmt.Println("6. Kembali ")
+		if x == 1 {
+			// punya dosen
+			fmt.Scan(&in)
+			switch in {
+			case 1:
+				// fmt.Println("add DOS")
+				inputUser(1)
+			case 2:
+				// del()
+				fmt.Println("del DOS")
+			case 3:
+				// update()
+				fmt.Println("up DOS")
+			case 4:
+				// fmt.Println("viewall DOS")
+				ReadAll(&dosen, &mahasiswa, 1)
+			case 5:
+				// viewID()
+				fmt.Println("view DOS")
+			}
+		} else if x == 2 {
+			// punya mahasiswa
+			fmt.Scan(&in)
+			switch in {
+			case 1:
+				// fmt.Println("add MHS")
+				inputUser(2)
+			case 2:
+				// del()
+				fmt.Println("del MHS")
+			case 3:
+				// update()
+				fmt.Println("up MHS")
+			case 4:
+				// fmt.Println("viewall MHS")
+				ReadAll(&dosen, &mahasiswa, 2)
+			case 5:
+				// viewID()
+				fmt.Println("view MHS")
+			}
+		}
 	}
 }
 
 func main() {
-	scanner := bufio.NewScanner(os.Stdin)
-	fmt.Println("Test")
-
-	for {
-
-		inputUser(1)
-
-		fmt.Print("Tambah Data Lagi ? ( y / n ) \n-> ")
-		scanner.Scan()
-		x := scanner.Text()
-
-		if x != "y" {
-			break
-		}
-
-		fmt.Print("\n")
-	}
-	inputUser(1)
-	ReadAll(&dosen, &mahasiswa, 1)
+	utama()
 }
